bcbdbbce1918715cd8908bb56e45f7b5eb299259
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2023-07-20 16:37:02 -0300

    requisito 10 feito

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -13,7 +13,21 @@ public class Rent
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+        DaysRented = daysRented;
+        Status = RentStatus.Confirmed;
+        vehicle.IsRented = true;
+        
+        if(person is PhysicalPerson)
+        {
+            Price = vehicle.PricePerDay* daysRented;
+        }
+        else
+        {
+            Price = vehicle.PricePerDay * daysRented * 0.9;
+        }
+        person.Debit += Price;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
